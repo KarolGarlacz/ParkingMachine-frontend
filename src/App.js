9ec7dedd4b5af8d8cb7a401b5174743dd010e1e9
Karol <karol.garlacz@gmail.com>
2021-06-28 10:31:09 +0200

    start parking machine application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Start</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+          <Route path="/" component={Home} />
+        </section>
+      </div>
+    </Router>
   );
 }
 
